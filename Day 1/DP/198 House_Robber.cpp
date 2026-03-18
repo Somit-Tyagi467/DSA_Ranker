5ad55c41959a5c95d0c96e39f5892f4657666e23
Somit Tyagi <somittyagi559@gmail.com>
2026-03-18 23:26:22 +0530

    Revision day 2

--- Day 1/DP/198 House_Robber.cpp
+++ Day 1/DP/198 House_Robber.cpp
@@ -33,3 +33,22 @@ public:
         return solve(0, nums,dp);
     }
 };
+
+//revision
+class Solution {
+public:
+    int solve(int i, vector<int>& arr , vector<int>& dp){
+        if(i >= arr.size())
+            return 0;
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int rob = arr[i] + solve(i+2, arr,dp);
+        int skip = solve(i+1, arr,dp);
+        return dp[i]=max(rob, skip);
+    }
+    int rob(vector<int>& nums) {
+        vector <int> dp(nums.size(),-1);
+        return solve(0, nums,dp);
+    }
+};
